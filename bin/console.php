0bd69a9a6fce3451641daf771bf638ee049ffb10
Ricardo Araujo <ricardo-araujo@forseti.com.br>
2018-01-24 16:18:09 -0200

    Correcao de namespaces

--- bin/console.php
+++ bin/console.php
@@ -6,8 +6,8 @@ use Symfony\Component\Console\Application;
 
 $app = new Application('New Empresometro', '1.0.0');
 
-$app->add($container->get(\Forseti\Empresometro\Command\CapturaEmpresaCommand::class));
-$app->add($container->get(\Forseti\Empresometro\Command\CapturaTodosCnpjsCommand::class));
-$app->add($container->get(\Forseti\Empresometro\Command\CapturaEmpresasFromTableCommand::class));
+$app->add($container->get(\Forseti\Cnpjs\Command\CapturaEmpresaCommand::class));
+$app->add($container->get(\Forseti\Cnpjs\Command\CapturaTodosCnpjsCommand::class));
+$app->add($container->get(\Forseti\Cnpjs\Command\CapturaEmpresasFromTableCommand::class));
 
 $app->run();
